a953cac8124292dfcbdf59a2a3fc4d6b2012a158
ShruthiVinay <48754950+ShruthiVinay@users.noreply.github.com>
2020-01-29 11:26:30 +0530

    modified file commit

--- src/pac/SampleTest.java
+++ src/pac/SampleTest.java
@@ -5,6 +5,8 @@ public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("no space here...");
 		System.out.println("spce adjusted for today");
+		
+		System.out.println("sir what about tommrow");
 	}
 
 }
